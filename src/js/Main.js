8bc3e17927184b9d33acedf84b1a4bdbd3194342
davybolivar <davy.bolivar@gmail.com>
2016-09-13 20:09:24 +0800

    Add 'scrollreveal' plugin

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import Footer from './Components/Footer';
+import ScrollReveal from 'scrollreveal'
 import 'mdi';
 import '../styles/css/Main.css';
 
 class Main extends Component {
+  componentDidMount(){
+    window.sr = ScrollReveal(( {
+        duration: 500,
+        scale: 0
+      } )
+    ).reveal('.title, .motto, .featured-game, .section-label, .game-list > li');
+    window.sr.reveal('.game-list > li', { duration: 1000 }, 50);
+  }
   render() {
     return (
       <div className="page-wrapper">
