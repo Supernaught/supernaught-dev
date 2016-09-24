1117a1de964ee116e00b46ec5ba29749cc188949
davybolivar <davy.bolivar@gmail.com>
2016-09-25 07:25:16 +0800

    Add route checker in Game.js

--- src/js/Pages/Game.js
+++ src/js/Pages/Game.js
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 import GameData from '../../datas/games.js';
+import { browserHistory } from 'react-router';
 import '../../styles/css/Game.css';
 
 class Game extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
-    // const gameDataList = GameData.games;
-    // const name = gameDataList.featured.filter((element) => {
-    //   return (element.id === this.props.params.game) ? true : false;
-    // });
-    // console.log(name[0]);
+
+    const gameData = getGame(this.props.params.game);
+
+    // If gameData is not found in games.js then redirect
+    if(gameData.length <= 0){
+      browserHistory.push('/');
+    }
   }
+
   render() {
-    const gameData = GameData.games.gameList.filter((element) => {
-      return (element.id === this.props.params.game) ? true : false;
-    });
+    const gameData = getGame(this.props.params.game);
 
-    console.log(gameData[0]);
-
-    return (
-      <div className="Game page-component">
-        <Header startY={50} />
-        <div
-          className="banner-wrapper"
-          style={{
-            backgroundImage: 'url("http://i.imgur.com/ibzBYL8.jpg")',
-            height: "350px"
-          }}>
-          <h1 className="title">{gameData[0].name}</h1>
-          <a className="play-button"
-             href={gameData[0].links[0].link}
-             target="*">
-             {gameData[0].links[0].name}
-          </a>
+    if(gameData[0]){
+      return (
+        <div className="Game page-component">
+          <Header startY={50} />
+          <div
+            className="banner-wrapper"
+            style={{
+              backgroundImage: 'url("http://i.imgur.com/ibzBYL8.jpg")',
+              height: "350px"
+            }}>
+            <h1 className="title">{gameData[0].name}</h1>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else{
+      return null;
+    }
   }
 }
 
 export default Game;
+
+function getGame(id){
+  const gameData = GameData.games.gameList.filter((element) => {
+    return (element.id === id) ? true : false;
+  });
+  return gameData;
+}
